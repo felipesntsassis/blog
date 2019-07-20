ee493ac21e7c26b659b2a94686aa4d822a5a1297
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2019-07-20 17:47:19 -0300

    Remoção dos códigos antigos das aulas.

--- src/main/java/br/com/escolpi/blog/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.escolpi.blog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.escolpi.blog.model.Post;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> { }
